f95d07d06deedf53eff2cc6a5bd1b585863d1a11
Jorge Lopez Sanchez <jorgels120878@gmail.com>
2022-12-14 17:05:37 -0600

    Primer commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hola Mundo");
+let mensaje = "Hola Mundo";
+mensaje = "Prueba";
+console.log(mensaje);
+mensaje = "Adios";
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log("Hola Mundo")
+let mensaje: string = "Hola Mundo"
+
+mensaje= "Prueba"
+console.log(mensaje)
+
+mensaje= "Adios"
